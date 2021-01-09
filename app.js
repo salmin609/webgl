7253bfb9b0671e5fc759b58b6e3349c07c8b4681
salmin609 <ryan95kr@gmail.com>
2021-01-09 18:37:45 +0900

    webgl tutorial 1

---
+++ app.js
@@ -0,0 +1,105 @@
+"use strict";
+
+function main() {
+    // Get A WebGL context
+    var canvas = document.querySelector("#c");
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        return;
+    }
+
+    // Use our boilerplate utils to compile the shaders and link into a program
+    var program = webglUtils.createProgramFromScripts(gl, ["vertexShader", "fragmentShader"]);
+
+    // look up where the vertex data needs to go.
+    var positionAttributeLocation = gl.getAttribLocation(program, "vertexPos");
+
+    // look up uniform locations
+    var resolutionUniformLocation = gl.getUniformLocation(program, "resolution");
+
+    // Color uniform locations
+    var colorUniformLocation = gl.getUniformLocation(program, "color");
+
+    // Create a buffer to put three 2d clip space points in
+    var positionBuffer = gl.createBuffer();
+
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    var positions = [
+        10, 20,
+        80, 20,
+        10, 30,
+        10, 30,
+        80, 20,
+        80, 30,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the attribute
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionAttributeLocation, size, type, normalize, stride, offset);
+
+    // set the resolution
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
+
+    for (var i = 0; i < 50; ++i) 
+    {
+        // draw
+        SetRectangle(gl, RandomInt(300), RandomInt(300), RandomInt(300), RandomInt(300));
+        gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+        gl.drawArrays(primitiveType, offset, count);
+    }
+
+}
+function RandomInt(range)
+{
+    return Math.floor(Math.random() * range);
+}
+
+function SetRectangle(gl, x, y, width, height)
+{
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2
+    ]),
+    gl.STATIC_DRAW);
+}
+
+main();
